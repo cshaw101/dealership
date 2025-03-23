be7dbb1403c16afea5c2057b6cd728d39e4be7f4
Caleb Shaw <shawcaleb1688@gmail.com>
2025-03-23 01:53:31 -0400

    working. need to fix database

--- car-dealership-frontend/src/components/Header.js
+++ car-dealership-frontend/src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
-        <Link to="/cart">Cart</Link>
+        <Link to="/login">Login</Link>
       </nav>
     </header>
   );
--- car-dealership-frontend/src/lib/supaBaseClient.js
+++ car-dealership-frontend/src/lib/supaBaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://pbfeehevtkmdlopdbkek.supabase.co'; // Replace with your Supabase URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBiZmVlaGV2dGttZGxvcGRia2VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI0MzAyMjAsImV4cCI6MjA1ODAwNjIyMH0.L327f3DXczNGTCljmAOAVTI8pOXz38Cc9lo2Wze98vY'; // Replace with your Supabase anon key
+const supabaseUrl = 'https://eizaaizbmoykhlxvkctv.supabase.co'; // Replace with your Supabase URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpemFhaXpibW95a2hseHZrY3R2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI3MDY5MzMsImV4cCI6MjA1ODI4MjkzM30.VlhSFOC5gfzD_UBGLhmWi-MS1j1YLZ3Ml5LDxRgOWRU'
 export const supabase = createClient(supabaseUrl, supabaseKey);
